21daf527d7913e8d4c40da5d091ac0c1356ddd9a
maxdegers <maxdegers@gmail.com>
2023-11-19 18:22:04 +0100

    creat file

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 18:01:45 by mbrousse          #+#    #+#             */
+/*   Updated: 2023/11/19 18:21:33 by mbrousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#define BUFFER_SIZE
+
+char	*get_next_line(int fd)
+{
+}
+
+int	main(int argc, char const *argv[])
+{
+	
+	return (0);
+}
